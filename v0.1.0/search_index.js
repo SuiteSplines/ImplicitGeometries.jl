7645e78e0dce140f9cee3b7bb0f10d4244a7017e
Documenter.jl <documenter@juliadocs.github.io>
2025-06-30 08:40:59 +0000

    build based on d1c2ee4

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ImplicitGeometries","page":"Home","title":"ImplicitGeometries","text":"","category":"section"},{"location":"#Index","page":"Home","title":"Index","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
